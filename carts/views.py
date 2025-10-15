6f6a56bc9fa553ccb0a74704765082bc8d73f11d
oneinonly <arunahirwar45@gmail.com>
2025-10-16 00:42:14 +0530

    production variation

--- carts/views.py
+++ carts/views.py
@@ -11,6 +11,11 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
+    # color=request.POST.get('color')
+    # size=request.POST.get('size')
+    # return HttpResponse(f"{color} and {size}")
+    # exit()
+    
     product = Product.objects.get(id=product_id) # get the product
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) # get the cart using the cart_id present in the session
